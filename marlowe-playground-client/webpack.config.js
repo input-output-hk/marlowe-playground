0111449c719e6405326cc1d0cae4a530a091794f
Hernan Rajchert <hrajchert@gmail.com>
2023-05-08 12:02:27 -0300

    Fix webpack proxy and https

--- marlowe-playground-client/webpack.config.js
+++ marlowe-playground-client/webpack.config.js
@@ -47,14 +47,11 @@ module.exports = {
       directory: path.join(__dirname, "dist"),
     },
     port: 8009,
-    https: !isDevelopment,
+    // HTTPS needs to be available for the GIST login to work
+    https: true,
     proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-      },
-      "/runghc": {
-        target: "http://localhost:8080",
-      },
+      "/api": "http://127.0.0.1:8080",
+      "/runghc": "http://127.0.0.1:8080",
     },
   },
   entry: "./entry.js",
