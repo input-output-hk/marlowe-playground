d61fcbe984ff5a407df2a7f5b34724e77628a801
Tomasz Rybarczyk <tomasz.rybarczyk@iohk.io>
2023-05-31 15:36:05 +0200

    Apply shellcheck suggestions

--- e2e/run_tests.sh
+++ e2e/run_tests.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Note: The reporter won't generate on failure, because it's in a post-cucumber hook,
 # and technically because of the way that node works, it won't call post-hooks,
 #  if the underlying script it's running, in our case, cucumber, fails. So we use
@@ -6,6 +7,5 @@
 
 #cucumber tag
 tag=$1
-offlineFlag=$2
 
 yarn run cucumber --profile "$tag"  || yarn run postcucumber
